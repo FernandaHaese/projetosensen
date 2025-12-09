6b7e2aeb10f6a5c4c8829eec657b90b943079f3a
BrenoT1 <breno200752@gmail.com>
2025-12-09 14:08:32 -0300

    consertei o import no i18n.ts

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import ptTranslations from '@/locales/pt.yml';
-import enTranslations from '@/locales/en.yml';
+import ptTranslations from './locales/pt.yml';
+import enTranslations from './locales/en.yml';
 
 const savedLanguage = localStorage.getItem('sensen-language') || 'pt';
 
